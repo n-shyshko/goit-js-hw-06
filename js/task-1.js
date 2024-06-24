fc754f45f64e3315692f97a2d48d92227194c940
nik <nshishko@gmail.com>
2024-06-24 14:00:13 +0300

    UPLOAD HW 6

--- js/task-1.js
+++ js/task-1.js
@@ -1,41 +1,30 @@
-// Variant 1
-// function isEnoughCapacity(products, containerSize) {
-//     let totalProducts = 0;
-//     for (let product in products) {
-//         if (products.hasOwnProperty(product)) {
-//             totalProducts += products[product];
-//         }
-//     }
-
-//     return totalProducts <= containerSize;
-// }
-
-// Variant 2
-function isEnoughCapacity(products, containerSize) {
-let totalProdAmount = 0;
-let singleProdAmount = Object.values(products); 
-    for (const product of singleProdAmount) {
-        totalProdAmount += product;
-        }
-    if (totalProdAmount <= containerSize) {
-        return true;
-    } 
-    return false;
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
 };
 
-
-console.log(
-    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-  ); // false
-  
-  console.log(
-    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-  ); // false
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
--- js/task-2.js
+++ js/task-2.js
@@ -1,41 +1,34 @@
-function calcAverageCalories(days) {
-    if (days.length === 0) {
-      return 0;
-    };
+class Storage {
+    #items;
+    constructor(items) {
+      this.#items = items;
+    }
+    getItems() {
+      return this.#items;
+    }
+    addItem(item) {
+      this.#items.push(item);
+    }
+    removeItem(itemToRemove) {
 
-    let totalCalories = 0;
-    for (let day of days) {
-        totalCalories +=day.calories;
-    };
+    const indexToDelete = this.#items.indexOf(itemToRemove);
 
-    const averageCalories = totalCalories / days.length;
-    return averageCalories;
-};
+    if (indexToDelete !== -1) {
+      this.#items.splice(indexToDelete, 1);
+    }
+  }
+}
+    
 
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 3010 },
-      { day: "tuesday", calories: 3200 },
-      { day: "wednesday", calories: 3120 },
-      { day: "thursday", calories: 2900 },
-      { day: "friday", calories: 3450 },
-      { day: "saturday", calories: 3280 },
-      { day: "sunday", calories: 3300 }
-    ])
-  ); // 3180
-  
-  console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 2040 },
-      { day: "tuesday", calories: 2270 },
-      { day: "wednesday", calories: 2420 },
-      { day: "thursday", calories: 1900 },
-      { day: "friday", calories: 2370 },
-      { day: "saturday", calories: 2280 },
-      { day: "sunday", calories: 2610 }
-    ])
-  ); // 2270
-  
-  console.log(
-    calcAverageCalories([])
-  ); // 0
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
